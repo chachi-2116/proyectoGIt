1ea5dee8f0530339bd1095aa3bd418c132c1b4d7
chachi-2116 <chachi211rojas@gmail.com>
2024-08-09 21:41:34 -0600

    se le agrego la segunda modificacion

--- CalculadoraTiempoViaje.java
+++ CalculadoraTiempoViaje.java
@@ -6,14 +6,14 @@ public class CalculadoraTiempoViaje {
         
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Ingrese la distancia entre las dos ciudades (en kilómetros): ");
-            double distancia = scanner.nextDouble();
+            double longitud = scanner.nextDouble();
 
             
             System.out.print("Ingrese la velocidad constante de la bicicleta (en km/h): ");
             double velocidad = scanner.nextDouble();
 
             
-            double duracion = distancia / velocidad;
+            double duracion = longitud / velocidad;
 
             
             System.out.println("El tiempo necesario para llegar de una ciudad a otra es: " + duracion + " horas");
